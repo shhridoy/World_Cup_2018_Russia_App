f18f8c9e05c40468fc6ce3ae90895091a96a886b
shhridoy <shhridoy.cste@gmail.com>
2018-01-12 12:37:40 +0600

    Initial commit

---
+++ app/src/main/java/com/shhridoy/worldcup2018russia/myPagerClasses/PagerAdapter.java
@@ -0,0 +1,45 @@
+package com.shhridoy.worldcup2018russia.myPagerClasses;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.shhridoy.worldcup2018russia.myTabFragments.TabFragment1;
+import com.shhridoy.worldcup2018russia.myTabFragments.TabFragment2;
+import com.shhridoy.worldcup2018russia.myTabFragments.TabFragment3;
+
+/**
+ * Created by Dream Land on 1/12/2018.
+ */
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    private int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                TabFragment1 tab1 = new TabFragment1();
+                return tab1;
+            case 1:
+                TabFragment2 tab2 = new TabFragment2();
+                return tab2;
+            case 2:
+                TabFragment3 tab3 = new TabFragment3();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
---
+++ app/src/main/java/com/shhridoy/worldcup2018russia/myTabFragments/TabFragment1.java
@@ -0,0 +1,21 @@
+package com.shhridoy.worldcup2018russia.myTabFragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shhridoy.worldcup2018russia.R;
+
+/**
+ * Created by Dream Land on 1/12/2018.
+ */
+
+public class TabFragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+    }
+}
