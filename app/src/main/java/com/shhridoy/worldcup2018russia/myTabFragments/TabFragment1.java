e9afbe8e7233cbdd9f774ae8636c2336c1cae20b
shhridoy <shhridoy.cste@gmail.com>
2018-01-13 15:47:13 +0600

    round chooser spinner added

--- app/src/main/java/com/shhridoy/worldcup2018russia/myTabFragments/TabFragment1.java
+++ app/src/main/java/com/shhridoy/worldcup2018russia/myTabFragments/TabFragment1.java
@@ -9,6 +9,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.shhridoy.worldcup2018russia.R;
 import com.shhridoy.worldcup2018russia.myRecyclerViewData.ListItems;
@@ -23,19 +27,39 @@ import java.util.List;
 
 public class TabFragment1 extends Fragment {
 
+    Spinner chooserSpinner;
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     List<ListItems> listItems;
+    ArrayAdapter<String> spinnerAdapter;
+    String[] spinnerItems;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_fragment_1, container, false);
 
+        chooserSpinner = rootView.findViewById(R.id.RoundChooserSpiner);
         recyclerView = rootView.findViewById(R.id.RecyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         listItems = new ArrayList<>();
+
+        spinnerItems = new String[]{"Round 1", "Round 2", "Round 3", "Round of 16", "Quarter-finals", "Semi-finals", "3rd Place Playoff", "Final"};
+        spinnerAdapter = new ArrayAdapter<String>(getContext(), R.layout.round_chooser_item, R.id.RoundChooserTV, spinnerItems);
+        chooserSpinner.setAdapter(spinnerAdapter);
+        chooserSpinner.setSelection(0);
+        chooserSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(getContext(), "Choose "+spinnerItems[position], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //spinnerFontSize.setSelection(1);
+            }
+        });
 
         ListItems item1 = new ListItems("Thu 14.06.18 21:00", "Round 1", "Russia", "Saudi Arab", "1 : 1");
         listItems.add(item1);
